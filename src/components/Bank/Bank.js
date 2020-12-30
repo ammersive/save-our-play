2166f32c0e8420328c4721ef502ebf5bfdaeb469
Sophie Stammers <sophie.stammers@gmail.com>
2020-12-30 18:45:24 +0000

    Component: Bank to class component in prep for API action

--- src/components/Bank/Bank.js
+++ src/components/Bank/Bank.js
@@ -1,34 +1,82 @@
+import { Component } from "react";
 import BankPickButton from "../BankPickButton/";
 import BankClearButton from "../BankClearButton/";
 
-const Bank = ({ bank }) => {
+class Bank extends Component {
+  constructor(props) {
+      super(props);
+
+  }
+
+  // componentDidMount() {
+  //   this.props.handleLoad();
+  // }
   
-  return (    
-    <div className="bank">
-      <h3>Player bank</h3> 
-      {bank.length > 0 ? 
-      <> 
-        <h4>Number of previous plays</h4> 
-        <ul>
-          { bank.map((player, index) => (
-            <li className={ player.isPicked === true ? "picked" : null }>
-              <div className="player-cell">
-                { player.name }
-              </div>  
-              <div className="play-count-cell">
-                { player.playCount }
-              </div> 
-              <div className="bank-add-wrap">
-                <BankPickButton index={ index } player={ player }/>
-              </div>
-            </li>   
-          )) }
-        </ul>
-        <BankClearButton buttonText={ "Clear the bank" }/>
-      </>
-      : <p className="no-players">Bank is empty</p> }      
-    </div>    
-  );
+  render() {
+    const { bank } = this.props;
+
+    return (    
+      <div className="bank">
+        <h3>Player bank</h3> 
+        {bank.length > 0 ? 
+        <> 
+          <h4>Number of previous plays</h4> 
+          <ul>
+            { bank.map((player, index) => (
+              <li className={ player.isPicked === true ? "picked" : null }>
+                <div className="player-cell">
+                  { player.name }
+                </div>  
+                <div className="play-count-cell">
+                  { player.playCount }
+                </div> 
+                <div className="bank-add-wrap">
+                  <BankPickButton index={ index } player={ player }/>
+                </div>
+              </li>   
+            )) }
+          </ul>
+          <BankClearButton buttonText={ "Clear the bank" }/>
+        </>
+        : <p className="no-players">Bank is empty</p> }      
+      </div>    
+    );
+  }  
 };
 
 export default Bank;
+
+
+
+
+// const Bank = ({ bank }) => {
+  
+//   return (    
+//     <div className="bank">
+//       <h3>Player bank</h3> 
+//       {bank.length > 0 ? 
+//       <> 
+//         <h4>Number of previous plays</h4> 
+//         <ul>
+//           { bank.map((player, index) => (
+//             <li className={ player.isPicked === true ? "picked" : null }>
+//               <div className="player-cell">
+//                 { player.name }
+//               </div>  
+//               <div className="play-count-cell">
+//                 { player.playCount }
+//               </div> 
+//               <div className="bank-add-wrap">
+//                 <BankPickButton index={ index } player={ player }/>
+//               </div>
+//             </li>   
+//           )) }
+//         </ul>
+//         <BankClearButton buttonText={ "Clear the bank" }/>
+//       </>
+//       : <p className="no-players">Bank is empty</p> }      
+//     </div>    
+//   );
+// };
+
+// export default Bank;
